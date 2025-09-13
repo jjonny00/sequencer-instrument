b1ecbbaa81c4b8e0ea4f559f5e2356e8b9c61bff
J Jonny <78575300+jjonny00@users.noreply.github.com>
2025-09-13 15:19:58 +0000

    removed use memo

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import * as Tone from "tone";
 
 import { startStarterLoop } from "./loop";
