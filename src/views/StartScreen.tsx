6c416501c1b8ee082b3f77e4d7d158d728288f17
J Jonny <78575300+jjonny00@users.noreply.github.com>
2025-10-06 13:20:40 -0400

    Enable scrolling on start screen

--- src/views/StartScreen.tsx
+++ src/views/StartScreen.tsx
@@ -34,7 +34,7 @@ export function StartScreen({
     >
       <TopBar left={topBarLeft} center={topBarCenter} right={topBarRight} />
       <div
-        className="flex-1 min-h-0 overflow-y-auto"
+        className="flex-1 min-h-0 overflow-y-auto scrollable"
         style={{
           flex: 1,
           minHeight: 0,
